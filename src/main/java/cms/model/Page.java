6d9c6ae33d470fa67cedf42eb0e5a10d5675354a
bencePal <paal.bence@gmail.com>
2018-02-04 18:47:26 +0100

    add basic db structure + spring run file

---
+++ src/main/java/cms/model/Page.java
@@ -0,0 +1,63 @@
+package cms.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "cms_page")
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToMany
+    private List<Category> categories;
+
+    private String title;
+
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
