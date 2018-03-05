37ee18de9333f917d55b6e679836e7234199dbe8
bencePal <paal.bence@gmail.com>
2018-03-05 20:24:48 +0100

    add roles

---
+++ src/main/java/cms/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package cms.repository;
+
+import cms.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByRole(String role);
+
+}
